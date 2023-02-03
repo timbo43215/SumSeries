5d0e5f27792351cd4565ae6cfefa10441d948a62
Tim Stack <tstack43215@yahoo.com>
2023-02-03 13:13:58 -0600

    Initial Commit

---
+++ SumSeries/SumSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumSeriesApp.swift
+//  SumSeries
+//
+//  Created by IIT PHYS 440 on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SumSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
